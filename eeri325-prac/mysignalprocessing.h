3f6b42fdf6e4359782389c8df342e89f28a7c35b
adam lt <adam.coetzee@gmail.com>
2020-12-04 20:13:38 +0200

    revo1

--- eeri325-prac/mysignalprocessing.h
+++ eeri325-prac/mysignalprocessing.h
@@ -10,7 +10,10 @@
 #include <QAudioDecoder>
 #include <QAudioDeviceInfo>
 #include <QtGlobal>
+#include <fftw3.h>
 
+#define REAL 0
+#define IMAG 1
 
 class mySignalProcessing
 {
@@ -20,10 +23,12 @@ public:
 
 using namespace std;
 vector<complex<double>> MyFFT(vector<complex<double>> &samples);
-QVector<double> My4kLPF(QVector<double> &samples);
-complex<double> myfftbin(int k, vector<complex<double>> &samples);
-vector<complex<double>> myfft(vector<complex<double>> &samples);
+vector<complex<double>> MyInvFFT(vector<complex<double>> &samples);
 QVector<double> myMagSpectrum(vector<complex<double>> &freqBins);
-
+QVector<double> myPhaseSpectrum(vector<complex<double>> &freqBins);
+QVector<double> My4kLPF(QVector<double> &samples);
+QVector<double> My4kHPF(QVector<double> &samples);
+void ftw_test_1d(int N, fftw_complex *input, fftw_complex *output);
+void ftw_run();
 
 #endif // MYSIGNALPROCESSING_H
